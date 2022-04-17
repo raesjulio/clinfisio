41cd8fd78ead59ad16f2c888ff1edec1b7af9da8
Julio <despairnatural@gmail.com>
2022-04-17 09:00:58 -0300

    Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,79 @@
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { ChangeEvent, FormEvent, FormEventHandler, useCallback, useEffect, useMemo, useState } from 'react'
+import ReactModal from 'react-modal';
+import { Spinner } from '../components/Spinner/Spinner';
 import { api } from '../services/api'
 import { supabase } from '../services/supabase';
-
+interface IValuesLogin {
+    email: string
+    password: string
+}
 export default function Login() {
-  const router = useRouter();
+    const router = useRouter();
+    const [values, setValues] = useState<IValuesLogin>({email: "", password:""})
+    const [isOpen, setIsOpen] = useState(false)
+    const [error, setError]= useState(false)
 
-    const handleLogin = async () => {
-        const data = {
-            "email": "abdiaspc@live.com",
-            "password": "123456"
+    const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (values?.email ==="" || values.password ==="") {
+            setError(true)
+
+            return
         }
+        setIsOpen(true)
+       
+        const { user, session, error } = await supabase.auth.signIn(values)
+        console.log(user, session, error);
 
-        const { user, session, error } = await supabase.auth.signIn(data)
         if (user) {
             authPainel()
+        }
+        if (error) {
+            setIsOpen(false)
+            setError(true)
         }
     }
     function authPainel() {
         router.push({
-          pathname: '/painel'
+            pathname: '/painel'
         });
-    
-      }
-    return (
-        <div>
 
-            <input type="text" />
-            <input type="text" />
-            <button onClick={handleLogin}>login</button>
-        </div>
+    }
+    const onChangeValues =(e:ChangeEvent<HTMLInputElement>)=>{
+
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+    return (
+        <main className='__login'>
+            <div>
+                <img src="/images/logo.png" alt="" />
+            </div>
+         
+            <form onSubmit={handleLogin} >
+                <input
+                    value={values?.email}
+                    onChange={onChangeValues}
+                    type="email"
+                    autoComplete='email'
+                    name='email'
+                    placeholder='Email' />
+                <input
+                    value={values?.password}
+                    name='password'
+                    autoComplete="current-password"
+                    onChange={onChangeValues}
+                    type="password"
+                    placeholder='Senha' />
+                       {error&&  <section className='__error'>
+                <h3>
+                    Credenciais Invalidas
+                </h3>
+            </section>}
+                <button type='submit'>Entrar</button>
+            </form>
+         
+            <Spinner isOpen={isOpen} />
+        </main>
     )
 }
