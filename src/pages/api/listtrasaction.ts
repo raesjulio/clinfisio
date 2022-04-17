a879a89d68cfeb6ae45c252ce998dbbb72cb20c0
Julio <despairnatural@gmail.com>
2022-04-17 10:55:18 -0300

    ordenar por data

--- src/pages/api/listtrasaction.ts
+++ src/pages/api/listtrasaction.ts
@@ -4,7 +4,7 @@ import { supabase } from "../../services/supabase";
 
 export default async (request: NextApiRequest, response: NextApiResponse) =>{
     if (request.method === "GET") {
-        const { data, error } = await supabase.from("transation").select("*")
+        const { data, error } = await supabase.from("transation").select("*").order('created_at', { ascending: false })
         if (data) {
             return response.status(200).json(data)
         }
