0167ab4ac66cf46e4fa002f6d3375ca672d9f1ca
Julio <despairnatural@gmail.com>
2022-04-20 02:35:42 -0300

    Alterando formato da data

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react"
 import { ITable, ITransaction } from "../../interface/interfaces"
 import { queryClient } from "../../services/queryClient"
 import styles from "./styles.module.scss"
 
-export const Table = ({handleTransaction}: ITable) => {
-  const transaction =  queryClient.getQueryData<ITransaction[]>("list")
-  
+export const Table = ({ handleTransaction }: ITable) => {
+  const transaction = queryClient.getQueryData<ITransaction[]>("list")
+
   return (
     <>
       <table className={styles.container}>
@@ -18,15 +19,15 @@ export const Table = ({handleTransaction}: ITable) => {
         </thead>
         <tbody>
           {transaction?.map(item => {
-            return <tr key={item.id} onClick={()=>handleTransaction(item)}>
+            return <tr key={item.id} onClick={() => handleTransaction(item)}>
               <td><div className={styles.title}>{item.title}</div></td>
-              <td className={item.type? styles.deposit: styles.withraw}
-              >{item.type? "":`- `}
-              {new Intl.NumberFormat("pt-BR", { style: 'currency', currency: 'BRL' }).format(item.price/100)}</td>
-              <td ><div className={styles.desc}>{item.description}</div></td>
-              <td>{new Intl.DateTimeFormat("pt-BR").format(Date.parse(item.created_at))}</td>
+              <td className={item.type ? styles.deposit : styles.withraw}
+              >{item.type ? "" : `- `}
+                {new Intl.NumberFormat("pt-BR", { style: 'currency', currency: 'BRL' }).format(item.price / 100)}</td>
+              <td ><div className={styles.desc}>{item.description}</div></td>            
+              <td>{new Intl.DateTimeFormat("pt-BR").format(new Date(item.created_at.slice(0,19)))}</td>
             </tr>
-          })}         
+          })}
         </tbody>
       </table></>
   )
